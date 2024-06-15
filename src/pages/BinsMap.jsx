0c452b1261ef1b02b9a7b56eb97b56ebe1ffe025
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-06-15 13:42:25 +0700

    get user position but there is still bug

--- src/pages/BinsMap.jsx
+++ src/pages/BinsMap.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./../styles.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
@@ -7,13 +7,37 @@ import MarkerClusterGroup from "react-leaflet-cluster";
 import { markers } from "./../components/Markers";
 
 const BinsMap = () => {
+
+  const [userPos, setUserPos] = useState({ latitude: -6.174032, longitude: 106.825981 })
+
   const customIcon = new Icon({
     iconUrl: require("./../components/icons/marker-bins.png"),
     iconSize: [35, 49]
   });
+
+  useEffect(() => {
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          console.log("POS", pos)
+          setUserPos({ latitude: pos.coords.latitude, longitude: pos.coords.longitude })
+        });
+      } else {
+        window.alert("Geolocation is not supported by this browser.")
+      }
+    }
+
+    getLocation()
+
+    console.log([userPos.latitude, userPos.longitude])
+
+  })
+
+
   return (
     <MapContainer
-      center={[-6.174032, 106.825981]}
+      center={[userPos.latitude, userPos.longitude]}
       zoom={14}
       scrollWheelZoom={true}
     >
@@ -24,7 +48,11 @@ const BinsMap = () => {
       <MarkerClusterGroup chunkedLoading>
         {markers.map((marker, idx) => (
           <Marker position={marker.geocode} icon={customIcon} key={idx}>
-            <Popup>Filled {Math.ceil((marker.filled / 170) * 100)}%</Popup>
+            {/* <Popup>Filled {Math.ceil((marker.filled / 170) * 100)}%</Popup> */}
+            {
+              console.log("NEW", [userPos.latitude, userPos.longitude])
+            }
+            <Popup>{toString(userPos)}</Popup>
           </Marker>
         ))}
       </MarkerClusterGroup>
