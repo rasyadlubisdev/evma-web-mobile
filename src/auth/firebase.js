1ab8df767315f0b85ff94d96019d712ebf4f91fe
rasyadlubisdev <rasyadloebisa@gmail.com>
2025-05-10 03:26:58 +0700

    feat: add web app

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEMQH93PWKWK6eErsdCdyvlXR681L5cBc",
-  authDomain: "martivi-web-app.firebaseapp.com",
-  projectId: "martivi-web-app",
-  storageBucket: "martivi-web-app.appspot.com",
-  messagingSenderId: "160527499121",
-  appId: "1:160527499121:web:5113edc09de2be659dc0d0"
+  apiKey: "AIzaSyBgRi1f5_8Bbe-N-m-8BARb9267ffB1bvU",
+  authDomain: "evma-web-app.firebaseapp.com",
+  projectId: "evma-web-app",
+  storageBucket: "evma-web-app.firebasestorage.app",
+  messagingSenderId: "1073351956182",
+  appId: "1:1073351956182:web:9d75c2a1c15bc40f845b77",
+  measurementId: "G-0CP7GJRYR7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export const auth=getAuth();
-export const db=getFirestore(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 export default app;
